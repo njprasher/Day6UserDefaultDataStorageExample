8b1b52f70d0ba36ec2e44b27d8a88cf50c8b52cb
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-04 19:39:03 -0400

    [Neeraj] implemented user defaults to save data

--- Day6UserDefaultDataStorageExample/LoginViewController.swift
+++ Day6UserDefaultDataStorageExample/LoginViewController.swift
@@ -12,11 +12,49 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var txtEmailID: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
-    @IBOutlet weak var switchRememberMe: UISwitch!
+    
     @IBOutlet weak var btnSignIN: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let userDefault = UserDefaults.standard
+        
+        //if value is there then show it
+        
+        if let email = userDefault.string(forKey: "userEmail")
+        {
+            print(email)
+        }
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func btnLogin(_ sender: UIButton) {
+        if self.txtEmailID.text == "admin@gmail.com" && self.txtPassword.text == "admin123"
+        {
+            
+        }else{
+            let alert = UIAlertController(title: "Invalid Credentials", message: "", preferredStyle: UIAlertController.Style.actionSheet)
+            let actionOk = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+            alert.addAction(actionOk)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func switchRememberMe(_ sender: UISwitch) {
+        if sender.isOn{
+           // save details
+            let userDefault = UserDefaults.standard
+            userDefault.setValue(txtEmailID.text, forKey: "userEmail")
+            userDefault.setValue(txtPassword.text, forKey: "userPassword")
+        }else{
+            // remove details
+            let userDefault = UserDefaults.standard
+            userDefault.setValue(nil, forKey: "userEmail")
+            userDefault.setValue(nil, forKey: "userPassword")
+
+        }
     }
     
     
--- Day6UserDefaultDataStorageExample/WelcomeViewController.swift
+++ Day6UserDefaultDataStorageExample/WelcomeViewController.swift
@@ -14,7 +14,8 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
